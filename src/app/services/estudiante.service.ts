311978108ccf99e2d406016ad98b03b403e0995e
jimmyacua <jimmyacua@hotmail.com>
2019-05-06 18:59:12 -0600

    muestra la lista de profes en consola

--- src/app/services/estudiante.service.ts
+++ src/app/services/estudiante.service.ts
@@ -4,6 +4,7 @@ import { Estudiante } from '../modelo/estudiante';
 import { Curso } from '../modelo/curso';
 import { Observable } from  'rxjs';
 import { tap } from  'rxjs/operators';
+import { Profesor } from '../modelo/profesor';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'homeStudent' })
@@ -16,32 +17,24 @@ const httpOptions = {
 
 export class EstudianteService {
   PHP_API_SERVER = "http://ec2-18-207-248-234.compute-1.amazonaws.com";
-  //PHP_API_SERVER = "http://127.0.0.1:8080";
   
   constructor(private httpClient : HttpClient) { }
 
   getCursos(estudiante:Estudiante): Observable<Curso[]>{
-    //let cedula = estudiante.cedula;
     let cursos:Curso[];
     return this.httpClient.post<Curso[]>(`${this.PHP_API_SERVER}/cursosEst.php`,{
     cedula: estudiante.cedula}, httpOptions)
       .pipe(tap(res => {
         cursos =  res["cursos"];
-        //console.log('recuperados');
       }));
-      /*let cursos = `${this.PHP_API_SERVER}/homeStudent.php`;
-      console.log(cursos);
-      return this.httpClient.get<Curso[]>(`${this.PHP_API_SERVER}/homeStudent.php`);*/
-      
   }
 
   public getProfesores(curso : Curso) {
+    let profesores:Profesor[];
     return this.httpClient.post<any>(`${this.PHP_API_SERVER}/profCurso.php`, {
       sigla: curso.sigla }, httpOptions)
     .pipe(tap(res => {
-      console.log(res);
-
-      return res;
+      profesores = res["profesores"];
     }));
   }
 }
