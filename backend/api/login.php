f60b9da08073e63f499bacff570fa549d0be9e50
José Fernando Quirós Montero <jfqm26@gmail.com>
2019-05-08 23:18:15 -0600

    Elimina código comentado en login.php

--- backend/api/login.php
+++ backend/api/login.php
@@ -57,28 +57,3 @@ if(isset($postdata) && !empty($postdata))
     return http_response_code(404);
   }
 }
-
-/*// Extract, validate and sanitize the id.
-$us = ($_GET['username'] !== null)? mysqli_real_escape_string($con, $_GET['username']) : false;
-$pw = ($_GET['pass'] !== null)? mysqli_real_escape_string($con, $_GET['pass']) : false;
-
-if(!$us || !$pw)
-{
-  return http_response_code(400);
-}
-
-// Call function
-$sql = "SELECT loginUsuario('{$us}','{$pw}') AS resultado";
-
-$result = mysqli_query($con,$sql);
-
-if($result)
-{
-  $res = mysqli_fetch_assoc($result);
-  echo json_encode($res);
-}
-else
-{
-  return http_response_code(404);
-}
-*/
