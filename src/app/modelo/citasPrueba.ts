6425cb19577893cc71e27db69f056f0c922ea201
Cristian Rodriguez <cristian.bujan@yahoo.com>
2019-05-28 16:43:20 -0600

    Un poco mas avanzado

--- src/app/modelo/citasPrueba.ts
+++ src/app/modelo/citasPrueba.ts
@@ -2,6 +2,7 @@ import { Estudiante } from './estudiante';
 import { Time } from '@angular/common';
 export class Cita  {
     nombre: string;
+    dia:string;
     horaInicio: Time;
     horaFinal: Time;
     estado: string;
