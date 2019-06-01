ed8b36fa1b35e9100e03962151e47aac9364e2df
pipo2310 <cristian.bujan@yahoo.com>
2019-05-31 21:59:25 -0600

    Errores de merge solucionados en caso de error correr npm install --save flatpickr angularx-flatpickr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { CalendarioEstudianteComponent } from './estudiante/calendario-estudiante/calendario-estudiante.component';
+import { CitaComponent } from './cita/cita.component';
 @NgModule({
   declarations: [
     AppComponent,
