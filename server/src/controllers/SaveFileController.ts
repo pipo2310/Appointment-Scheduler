99a1ffc1be098e5da4ed56eee36e99dd2f58c47c
DilianBM <dilian,badilla@ucr.ac.cr>
2019-07-09 17:52:05 -0600

    commit

--- server/src/controllers/SaveFileController.ts
+++ server/src/controllers/SaveFileController.ts
@@ -2,7 +2,7 @@ import{Request,Response} from 'express';
 //import{manejarEventosCalendario} from '../../build/controllers/manejarEventosCalendario'
 import { Injectable } from '@angular/core';
 import { stringify } from '@angular/core/src/util';
-
+import pool from '../database';
  class saveArch {
   //composeOptions:email.ComposeOptions;
   constructor() {
@@ -11,6 +11,19 @@ import { stringify } from '@angular/core/src/util';
 
   saveArchivo(req:Request, res:Response){
     //getAsBinary();
+    let file;
+    file=req.body.formData.getAsBinary();
+  /*  let sql = "insert into insertDispDiasSemanaRangoFechas('"+cedula+"', '"+diaIni+"', '"+diaFin+"', '"+horaIni+"', '"+horaFin+"', '"+lugar+"', '"+lun+"', '"+mar+"', '"+mie+"', '"+jue+"', '"+vie+"', '"+sab+"')";
+    pool.query(sql, (err:Error, result:any) =>{
+        if(result){
+        res.send(result)
+        } else{
+            res.send({
+                "Error: ": err
+            })
+        }   
+    });*/
+
   }
 
  
